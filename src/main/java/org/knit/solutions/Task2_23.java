7d65dd5eef654cce1405593da981bec4775c15bf
dlozereg <dlozereg@gmail.com>
2025-06-06 21:03:58 +0400

    Task 23 complete

---
+++ src/main/java/org/knit/solutions/Task2_23.java
@@ -0,0 +1,26 @@
+package org.knit.solutions;
+
+import org.knit.TaskDescription;
+
+/*
+Лабораторная работа часть 2 № 10 Задача 23
+Тема: Создание веб-интерфейса для REST API с использованием Thymeleaf и Bootstrap
+
+16 баллов
+Цель работы
+
+Научиться создавать веб-интерфейс для REST API с использованием:
+
+    Thymeleaf для динамического рендеринга HTML
+    Bootstrap для стилизации страниц
+    Spring MVC для обработки запросов
+    Обработки ошибок через кастомные страницы
+
+ */
+@TaskDescription(taskNumber = 23, taskDescription = "Задание 23: Создание веб-интерфейса для REST API с использованием Thymeleaf и Bootstrap")
+public class Task2_23 implements Solution {
+    @Override
+    public void execute() {
+        System.out.println("Ссылка на проект: https://github.com/Dlozereg/Lab_2_10.git");
+    }
+}
